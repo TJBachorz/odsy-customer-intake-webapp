da52b14146048220839d8f1b85d4bc0a08927b63
TJ <tjbachorz@gmail.com>
2022-02-05 16:18:14 -0700

    adds customer table headers

--- src/components/CustomerTable.js
+++ src/components/CustomerTable.js
@@ -1,7 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import {
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from '@mui/material';
 
 const CustomerTable = () => {
 
+    const CUSTOMER_TABLE_HEADERS = ['fullName', 'email', 'vehicle_name', 'vehicle_type', 'vehicle_length'];
     const [customers, setCustomers] = useState([]);
 
     useEffect(() => getCustomers(), []);
@@ -11,8 +21,36 @@ const CustomerTable = () => {
         setCustomers(await customersResponse.json());
     };
 
+    const renderCustomerTableHeaders = () => {
+        return CUSTOMER_TABLE_HEADERS.map(header => {
+            return (
+                <TableCell align="left">{header}</TableCell>
+            )
+        })
+    }
+
+    const renderCustomerRows = () => {
+        return customers.map((customer, index) => {
+            return (
+                <CustomerTableRow key={index} customer={customer}/>
+            );
+        })
+    }
+
     return (
         <div className='main-container'>
+            <TableContainer component={Paper}>
+                <Table size="medium">
+                    <TableHead>
+                        <TableRow>
+                            {renderCustomerTableHeaders()}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {renderCustomerRows()}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     );
 }
