a0cb775e35f6bad9ca869140eb25c589486fa3d4
TJ <tjbachorz@gmail.com>
2022-02-05 18:08:49 -0700

    adds header component with mock upload button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 
 import CustomerTable from './components/CustomerTable';
+import Header from './components/Header';
 
 const App = () => {
 
   return (
     <div id="app-container">
+      <Header/>
       <CustomerTable/>
-      <div>FILE UPLOAD PLACEHOLDER</div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header id="odsy-header">
+            <div>Outdoor.sy</div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <button id="file-upload-button">
+                Upload File
+            </button>
+        </header>
+    );
+}
+
+export default Header;
